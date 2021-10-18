ef25e9d0a2802a7301a6c194fb9108e4f88453ad
junsung9889 <80762743+junsung9889@users.noreply.github.com>
2021-10-18 16:13:06 +0900

    Delete JsWebsite directory

--- JsWebsite/js/dash.js
+++
@@ -1,9 +0,0 @@
-var url = window.location.href;
-var nav = url.slice(window.location.href.indexOf('?') + 1);
-nav = nav.split('=');
-nav = nav[1];
-
-if (nav == 'warningKPI') {
-  $('.rows').hide();
-  $('.red').show();
-}
